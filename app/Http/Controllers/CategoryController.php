1f7699e251ed50090f3d082b87b8581ff50b98e2
leandroscasto <leandro.castro@grupomult.com.br>
2020-08-31 19:52:13 +0000

    Adicionando rotas, e entidades de categorias e generos.

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+
+    private $rules = [
+        'name' => 'required|max:255',
+        'is_active' => 'boolean'
+    ];
+
+    public function index(Request $request) //?only_trashed (dados removidos)
+    {
+        if ($request->has('only_trashed')) {
+            return Category::withTrashed()->get();
+        }
+        return Category::all();
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, $this->rules);
+        return Category::create($request->all());
+    }
+
+    public function show(Category $category)
+    {
+        return $category;
+    }
+
+    public function update(Request $request, Category $category)
+    {
+        $this->validate($request, $this->rules);
+        $category->update($request->all());
+        return $category;
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return response()->noContent();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group(['namesapce' => 'Api'], function () {
+    Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
+    Route::resource('genres', 'GenreController', ['except' => ['create', 'edit']]);
+});
